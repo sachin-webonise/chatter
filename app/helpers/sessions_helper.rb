1dc93f203f74559050f4ec352f387391d55a3966
sachin.webonise <sachin.wagh@weboniselab.com>
2014-01-10 21:50:28 +0530

    Added user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,4 +43,14 @@ module SessionsHelper
     session[:return_to] = request.url if request.get?
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
 end
